57caf5fac2fee3034489b6191defbcb229b6e159
佟晓芳 <tongxiaofang@tongxiaofangdeMacBook-Pro.local>
2018-12-13 15:31:36 +0800

    first commit

---
+++ src/renderer/components/login.vue
@@ -0,0 +1,26 @@
+<template>
+  <div id="login">
+    <el-input
+      placeholder="请输入内容"
+      v-model="loginValue"
+      clearable
+    >
+    </el-input>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'login',
+  data () {
+    return {
+      loginValue: ''
+    }
+  },
+  methods: {}
+}
+</script>
+
+<style lang="less" scoped>
+</style>
+
